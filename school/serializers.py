dcca407155a27e0479b4ca7e5980506b73c284cf
LongPhanPip <phanlonghd5200@gmail.com>
2021-05-01 17:08:22 +0700

    Add student_id field classroom serializer

--- school/serializers.py
+++ school/serializers.py
@@ -5,10 +5,11 @@ from teachers.serializers import TeacherSerializer
 
 class ClassroomSerializer(serializers.ModelSerializer):
     homeroom_teacher_id = serializers.IntegerField()
+    student_id = serializers.PrimaryKeyRelatedField(source='students', many=True, read_only=True)
 
     class Meta:
         model = Classroom
-        fields = ['id', 'class_name', 'homeroom_teacher_id', 'location']
+        fields = ['id', 'class_name', 'homeroom_teacher_id', 'location', 'student_id']
 
     def create(self, validated_data):
         return Classroom.objects.create(**validated_data)
